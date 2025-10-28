d4fec2b6377fc01e272ea62cf134f760fc5f2a75
Natoria Milligan <natoriaray@gmail.com>
2025-10-28 06:12:53 -0500

    added modal to show hidden account details

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,20 +1,40 @@
 import { useState } from 'react';
-import { Button, Collapse, Container, Card } from 'react-bootstrap';
+import { Button, Container, Card, Modal } from 'react-bootstrap';
 import Sidebar from './Sidebar';
 import '../Dashboard.css';
 
 function Dashboard() {
-    const [open, setOpen] = useState(false);
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true)
 
     return (
         <>
             <Sidebar />
-            <Container fluid>
+            <Container>
                 <div className="header-container">
-                    <h1 className="display-3">Hello John!</h1>
+                    <h2 className="display-3">Hello John!</h2>
                     <p className="acct-display">Checking ...8809</p>
                 </div>
-                
+                <Card>
+                    <Card.Body className="acct-balance">
+                        <h2 className="display-1">$782.38</h2>
+                        <button className="acct-details" onClick={handleShow}>Account Details</button>
+                    </Card.Body>
+                </Card>
+                <Modal show={show} onHide={handleClose}>
+                    <Modal.Header>
+                        <Modal.Title>Account Details</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <p>Rounting number: 1234567896</p>
+                        <p>Account number: 789456123000</p>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button onClick={handleClose}>Close</Button>
+                    </Modal.Footer>
+                </Modal>
             </Container>
         </>
 
