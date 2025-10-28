ed12f151e58c4dd48d4273cc2bf74bb1ab930adb
Natoria Milligan <natoriaray@gmail.com>
2025-10-28 15:18:52 -0500

    added Route for profile component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,21 @@ import './App.css';
 import { Container } from 'react-bootstrap';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/login';
+import Create from './components/Create';
+import Logout from './components/Logout';
+import Dashboard from './components/Dashboard';
+import Profile from './components/Profile';
 
 function App() {
   return (
-    <Container fluid>
+    <Container fluid className="px-0">
       <Router>
         <Routes>
           <Route path='/login' element={<Login />} />
+          <Route path='/create' element={<Create />} />
+          <Route path='/logout' element={<Logout />} />
+          <Route path='/dashboard' element={<Dashboard />} />
+          <Route path='/profile' element={<Profile />} />
         </Routes>
       </Router>
     </Container>
