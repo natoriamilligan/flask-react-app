ad74ae1945ae651f0f2ac7d618207e11570087c9
Natoria Milligan <natoriaray@gmail.com>
2025-11-03 09:32:12 -0600

    connected deposit component to API

--- frontend/src/components/Deposit.js
+++ frontend/src/components/Deposit.js
@@ -1,13 +1,39 @@
 import { useState } from 'react';
 import { Card, Button, Form } from 'react-bootstrap';
+import { jwtDecode } from 'jwt-decode';
 import '../Deposit.css';
 import Header from './Header';
 
 function Deposit() {
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const response = fetch("")
+    const [deposit, setDeposit] = useState('');
+
+    const token = localStorage.getItem("accessToken");
+    let accountId = null;
+
+    if (token) {
+        const decodedToken = jwtDecode(token);
+        accountId = decodedToken.sub || decodedToken.identity;
     }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const response = await fetch(`http://localhost:5000/account/${accountId}/deposit`, {
+            method: 'POST',
+            headers: {
+                'Content-Type' : 'application/json',
+                Authorization : `Bearer ${token}`
+            },
+            body: JSON.stringify({
+                amount: deposit
+            })
+        })
+
+        if (response.ok) {
+            alert("yes")
+        } else {
+            alert("no")
+        }
+    };
 
     return (
         <>
@@ -18,7 +44,11 @@ function Deposit() {
                     <Form onSubmit={handleSubmit}>
                         <Form.Group className="deposit-input">
                             <Form.Label className="fs-4">Amount:</Form.Label>
-                            <Form.Control type="text"></Form.Control>
+                            <Form.Control 
+                            type="text"
+                            value={deposit}
+                            onChange={(e) => setDeposit(e.target.value)}
+                            ></Form.Control>
                             <Form.Text muted>Daily limit $5,000</Form.Text>
                         </Form.Group>
                         <div className="deposit-btn">
