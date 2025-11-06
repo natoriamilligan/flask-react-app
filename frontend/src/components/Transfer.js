f3d5f7b23d05a9e186f34d1770e1c2f32cf5821d
Natoria Milligan <natoriaray@gmail.com>
2025-11-06 09:27:57 -0600

    added error handling for recipient inputs

--- frontend/src/components/Transfer.js
+++ frontend/src/components/Transfer.js
@@ -11,6 +11,8 @@ function Transfer() {
     const [memo, setMemo] = useState('');
     const [empty, setEmpty] = useState(false);
     const [isInvalid, setIsInvalid] = useState(false);
+    const [success, setSuccess] = useState(false);
+    const [notFound, setNotFound] = useState(false);
     const navigate = useNavigate();
 
     const toDashboard = () => {
@@ -28,7 +30,7 @@ function Transfer() {
     const handleSubmit = async (e) => {
         e.preventDefault();
 
-        if (empty == true) {
+        if (recipient == "") {
             setEmpty(true);
         } else {
             const response = await fetch(`http://localhost:5000/transfer`, {
@@ -46,10 +48,18 @@ function Transfer() {
             })
 
             if (response.ok) {
-            alert("yes")
-        } else {
-            alert("no")
-        }
+                alert("yes")
+            } else {
+                const data = await response.json();
+
+                if (data["code"] == 404) {
+                    setEmpty(false);
+                    setNotFound(true);
+                } else {
+                    setIsInvalid(true);
+                }
+                
+            }
         }
     };
 
@@ -59,42 +69,57 @@ function Transfer() {
             <Card className="form-card">
                 <Card.Header>Create Transfer</Card.Header>
                 <Card.Body>
-                    <Form onSubmit={handleSubmit}>
-                        <Form.Group className="form-input">
-                            <Form.Label className="fs-4">Amount:</Form.Label>
-                            <Form.Control 
-                            type="text"
-                            value={transfer}
-                            onChange={(e) => setTransfer(e.target.value)}
-                            />
-                            <Form.Text muted>Daily limit $5,000</Form.Text>
-                        </Form.Group>
-                        <Form.Group>
-                            <Form.Label className="fs-5">Recipient Account Number:</Form.Label>
-                            <Form.Control 
-                            type="text"
-                            value={recipient}
-                            onChange={(e) => setRecipient(e.target.value)}
-                            />
-                            {empty &&
-                                    <Form.Text muted>You must enter a recipient account number.</Form.Text>    
-                                }
-                        </Form.Group>
-                        <Form.Group>
-                            <Form.Label className="fs-5">Memo:</Form.Label>
-                            <Form.Control 
-                            type="text"
-                            value={memo}
-                            onChange={(e) => setMemo(e.target.value)}
-                            />
-                        </Form.Group>
-                        <div className="submit-btn">
-                            <Button type="submit">Transfer</Button>
-                        </div>
-                    </Form>
-                    <div className="cancel-btn">
-                        <Button variant="danger">Cancel Transfer</Button>
-                    </div>
+                    {success ? (
+                        <>
+                            <p>Transfer was successful!</p>
+                            <Button onClick={toDashboard}>Back to Dashboard</Button>
+                        </>
+                    ) : (
+                        <>
+                            <Form onSubmit={handleSubmit}>
+                                <Form.Group className="form-input">
+                                    <Form.Label className="fs-4">Amount:</Form.Label>
+                                    <Form.Control 
+                                    type="text"
+                                    value={transfer}
+                                    onChange={(e) => setTransfer(e.target.value)}
+                                    />
+                                    <Form.Text muted>Daily limit $5,000</Form.Text><br />
+                                    {isInvalid &&
+                                        <Form.Text>Please enter a valid amount.</Form.Text>
+                                    }
+                                </Form.Group>
+                                <Form.Group>
+                                    <Form.Label className="fs-5">Recipient Account Number:</Form.Label>
+                                    <Form.Control 
+                                    type="text"
+                                    value={recipient}
+                                    onChange={(e) => setRecipient(e.target.value)}
+                                    />
+                                    {empty &&
+                                            <Form.Text muted>You must enter a recipient account number.</Form.Text>    
+                                    }
+                                    {notFound &&
+                                            <Form.Text muted>Please enter a valid account number.</Form.Text>    
+                                    }
+                                </Form.Group>
+                                <Form.Group>
+                                    <Form.Label className="fs-5">Memo:</Form.Label>
+                                    <Form.Control 
+                                    type="text"
+                                    value={memo}
+                                    onChange={(e) => setMemo(e.target.value)}
+                                    />
+                                </Form.Group>
+                                <div className="submit-btn">
+                                    <Button type="submit">Transfer</Button>
+                                </div>
+                            </Form>
+                            <div className="cancel-btn">
+                                <Button variant="danger">Cancel Transfer</Button>
+                            </div>
+                        </>
+                    )}
                 </Card.Body>
             </Card>
         </>
