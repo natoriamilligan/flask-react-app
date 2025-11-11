387cc69758040ea622e41a09a3d7f1af37cc03fe
Natoria Milligan <natoriaray@gmail.com>
2025-11-11 11:26:56 -0600

    removed unnessasary imports

--- frontend/src/components/Transactions.js
+++ frontend/src/components/Transactions.js
@@ -1,24 +1,19 @@
 import { useState } from 'react';
 import { Container, ListGroup } from 'react-bootstrap';
-import Sidebar from './Sidebar';
-import Header from './Header';
 
 function Transactions() {
      return (
         <>
-            <Sidebar />
-            <Header />
-            <Container>
-                <ListGroup>
-                    <ListGroup.Item className="trans-list-item">
-                        <div className="list-item-top">11/34/25</div>
-                        <div className="list-item-bottom">
-                            <div>Deposit</div>
-                            <div>+$500</div>
-                        </div>
-                    </ListGroup.Item>
-                </ListGroup>
-            </Container>
+            <ListGroup>
+                <ListGroup.Item className="trans-list-item">
+                    <div className="list-item-top">11/34/25</div>
+                    <div className="list-item-bottom">
+                        <div>Deposit</div>
+                        <div>+$500</div>
+                    </div>
+                </ListGroup.Item>
+            </ListGroup>
         </>
      )
 }
+export default Transactions
