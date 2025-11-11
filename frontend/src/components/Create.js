21625a5b6aab6ca6f3c6427faea3f102e6150203
Natoria Milligan <natoriaray@gmail.com>
2025-11-11 10:33:44 -0600

    added a screen to show account creation was a success

--- frontend/src/components/Create.js
+++ frontend/src/components/Create.js
@@ -9,6 +9,7 @@ function Create() {
     const [lastname, createLastname] = useState('');
     const [username, createUsername] = useState('');
     const [password, createPassword] = useState('');
+    const [isCreated, setIsCreated] = useState(false);
     const [createError, setCreateError] = useState(false);
 
     const btnLink = () => {
@@ -32,7 +33,7 @@ function Create() {
                   })
               })
               if (response.ok) {
-                navigate("/dashboard");
+                setIsCreated(true);
               } 
           } catch {
               alert("Something wrong with the server");
@@ -45,50 +46,60 @@ function Create() {
         <Card bg="light" className="create-card">
           <Card.Header>Banksie</Card.Header>
           <Card.Body>
-            <Card.Title>Create Account</Card.Title>
-            <Form onSubmit={handleSubmit}>
-              <Form.Group controlId='firstname'>
-                <Form.Label>First Name:</Form.Label>
-                <Form.Control 
-                  type='text' 
-                  value={firstname}
-                  onChange={(e) => createFirstname(e.target.value)}
-                />
-              </Form.Group>
-              <Form.Group controlId='lastname'>
-                <Form.Label>Last Name:</Form.Label>
-                <Form.Control 
-                  type='text' 
-                  value={lastname}
-                  onChange={(e) => createLastname(e.target.value)}
-                />
-              </Form.Group>
-              <Form.Group controlId='username'>
-                <Form.Label>Username:</Form.Label>
-                <Form.Control 
-                  type='text' 
-                  value={username}
-                  onChange={(e) => createUsername(e.target.value)}
-                />
-              </Form.Group>
-              <Form.Group controlId='password'>
-                <Form.Label>Password:</Form.Label>
-                <Form.Control 
-                  type='text' 
-                  value={password}
-                  onChange={(e) => createPassword(e.target.value)}
-                />
-                {createError &&
-                  <Form.Text>Please complete all fields.</Form.Text>
-                }
-              </Form.Group>
+            {isCreated ? (
+              <>
+                <p>You've successfully created an account! Please log in.</p>
+                <Button onClick={btnLink}>Log in</Button>
+              </>
+            ) : (
+              <>
+              <Card.Title>Create Account</Card.Title>
+              <Form onSubmit={handleSubmit}>
+                <Form.Group controlId='firstname'>
+                  <Form.Label>First Name:</Form.Label>
+                  <Form.Control 
+                    type='text' 
+                    value={firstname}
+                    onChange={(e) => createFirstname(e.target.value)}
+                  />
+                </Form.Group>
+                <Form.Group controlId='lastname'>
+                  <Form.Label>Last Name:</Form.Label>
+                  <Form.Control 
+                    type='text' 
+                    value={lastname}
+                    onChange={(e) => createLastname(e.target.value)}
+                  />
+                </Form.Group>
+                <Form.Group controlId='username'>
+                  <Form.Label>Username:</Form.Label>
+                  <Form.Control 
+                    type='text' 
+                    value={username}
+                    onChange={(e) => createUsername(e.target.value)}
+                  />
+                </Form.Group>
+                <Form.Group controlId='password'>
+                  <Form.Label>Password:</Form.Label>
+                  <Form.Control 
+                    type='text' 
+                    value={password}
+                    onChange={(e) => createPassword(e.target.value)}
+                  />
+                  {createError &&
+                    <Form.Text>Please complete all fields.</Form.Text>
+                  }
+                </Form.Group>
+                <div className="card-btn">
+                  <Button type="submit" >Create Account</Button>
+                </div>
+              </Form>
               <div className="card-btn">
-                <Button type="submit" >Create Account</Button>
+                <Button variant="secondary" onClick={btnLink}>Log in</Button>
               </div>
-            </Form>
-            <div className="card-btn">
-              <Button variant="secondary" onClick={btnLink}>Log in</Button>
-            </div>
+            </>
+            )}
+              
           </Card.Body>
         </Card>
       </Container>
