e5ac2bd8720ea4766c7b62aa725f3edbc47c2e99
Natoria Milligan <natoriaray@gmail.com>
2025-11-11 11:23:36 -0600

    separated sent and recieved transfers to match account model

--- backend/resources/transactions.py
+++ backend/resources/transactions.py
@@ -1,29 +1,37 @@
 from flask.views import MethodView
 from flask_smorest import Blueprint
 
-from models import TransferModel, DepositModel, WithdrawalModel
+from models import TransferModel, DepositModel, WithdrawalModel, AccountModel
 from schemas import TransactionSchema, DepositSchema, WithdrawalSchema, TransferSchema
 
 blp = Blueprint("transactions", __name__, description="Operation on transactions")
 
-@blp.route("/transactions")
+@blp.route("/account/<int:account_id>/transactions")
 class TransactionList(MethodView):
     @blp.response(200, TransactionSchema(many=True))
-    def get(self):
-        deposits = DepositModel.query.all()
-        withdrawals = WithdrawalModel.query.all()
-        transfers = TransferModel.query.all()
+    def get(self, account_id):
+        account = AccountModel.query.get_or_404(account_id)
+
+        deposits = account.deposits.all()
+        withdrawals = account.withdrawals.all()
+        sent_transfers = account.sent_transfers.all()
+        received_transfers = account.received_transfers.all()
 
         deposit_data = DepositSchema(many=True).dump(deposits)
         withdrawal_data = WithdrawalSchema(many=True).dump(withdrawals)
-        transfer_data = TransferSchema(many=True).dump(transfers)
+        sent_transfer_data = TransferSchema(many=True).dump(sent_transfers)
+        received_transfer_data = TransferSchema(many=True).dump(received_transfers)
 
         for d in deposit_data:
             d["type"] = "Deposit"
         for w in withdrawal_data:
             w["type"] = "Withdrawal"
-        for t in transfer_data:
-            t["type"] = "Transfer"
+        for s in sent_transfer_data:
+            s["type"] = "Sent Transfer"
+        for r in received_transfer_data:
+            r["type"] = "Received Transfer"
+
+        transfer_data = sent_transfer_data + received_transfer_data
 
         transactions = deposit_data + withdrawal_data + transfer_data
         transactions.sort(key=lambda x: x["timestamp"], reverse=True)
