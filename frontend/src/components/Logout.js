6c3ccfb213fcb17548e0a7e0eb423dcf69d15dfe
Natoria Milligan <natoriaray@gmail.com>
2025-11-23 09:34:40 -0600

    deleted spaces

--- frontend/src/components/Logout.js
+++ frontend/src/components/Logout.js
@@ -1,12 +1,19 @@
-import { useState } from 'react';
-import { Card, Button, Form } from 'react-bootstrap';
+import { Card, Button } from 'react-bootstrap';
+import '../Logout.css';
+import { useNavigate } from 'react-router-dom';
 
 function Logout() {
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        navigate("/login")
+    }
     return (
-        <Card>
+        <Card className="card-wrapper">
             <Card.Header bg="light">Banksie</Card.Header>
-            <Card.Body>
+            <Card.Body className="body-wrapper">
                 <Card.Text>You have been successfully logged out.</Card.Text>
+                <Button className="login-btn" onClick={handleLogout}>Login</Button>
             </Card.Body>
         </Card>
     )
