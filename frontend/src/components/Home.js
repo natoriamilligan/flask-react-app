280b49ee32ab6c95f59f14af67c05e66a851ea5e
Natoria Milligan <natoriaray@gmail.com>
2025-11-23 09:23:42 -0600

    added home page

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+import { Button } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
+import '../Home.css';
+
+function Home() {
+const navigate = useNavigate();
+
+const toLogin = () => {
+    navigate("/Login")
+}
+
+const toCreate = () => {
+    navigate("/Create")
+}
+
+    return (
+        <div className="wrapper">
+            <h1>BANKSIE</h1>
+            <div className="btn-wrapper">
+                <Button className="front-btns" onClick={toLogin}>Login</Button>
+                <Button className="front-btns" onClick={toCreate}>Create Account</Button>
+                <Button className="front-btns">See Architecture Diagram</Button>
+            </div>
+      </div>
+    )
+}
+export default Home
