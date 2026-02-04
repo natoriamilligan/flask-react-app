45e7a1b81a435c6cd99bdb4a91b6d26c3ef42959
Natoria Milligan <natoriaray@gmail.com>
2026-02-04 10:52:24 -0600

    improved error handling

--- backend/resources/deposits.py
+++ backend/resources/deposits.py
@@ -5,7 +5,7 @@ from flask_jwt_extended import jwt_required
 
 from db import db
 from models import AccountModel, DepositModel
-from schemas import DepositSchema, AccountSchema
+from schemas import DepositSchema
 
 blp = Blueprint("deposits", __name__, description="Operation on deposits")
 
@@ -13,19 +13,23 @@ blp = Blueprint("deposits", __name__, description="Operation on deposits")
 class AccountDeposit(MethodView):
     @blp.response(200, DepositSchema(many=True))
     def get(self, account_id):
-        account = AccountModel.query.get_or_404(account_id)
+        account = AccountModel.query.get(account_id)
+
+        if not account:
+            return {"message": "Account not found."}, 404
+        
         return account.deposits.all()
     
     @jwt_required()
     @blp.arguments(DepositSchema)
-    @blp.response(200, DepositSchema)
     def post(self, deposit_data, account_id):
-        if AccountModel.query.filter(AccountModel.id == account_id).first():
-            account = AccountModel.query.get_or_404(account_id)
-            account.balance = account.balance + deposit_data["amount"]
-        else:
-            abort(404, message="Recipient not found")
 
+        account = AccountModel.query.get(account_id)
+
+        if not account:
+            return {"message": "Account not found."}, 404
+        
+        account.balance = account.balance + deposit_data["amount"]
         deposit = DepositModel(account_id=account_id, **deposit_data)
         
         try:
@@ -34,13 +38,17 @@ class AccountDeposit(MethodView):
         except SQLAlchemyError:
             abort(500, message="An error occured adding the deposit to the database")
 
-        return deposit
+        return {"message": "Deposit was successfully posted."}, 201
     
 @blp.route("/deposit/<int:deposit_id>")
 class Deposit(MethodView):
     @blp.response(200, DepositSchema)
     def get(self, deposit_id):
-        deposit = DepositModel.query.get_or_404(deposit_id)
+        deposit = DepositModel.query.get(deposit_id)
+
+        if not deposit:
+            return {"message": "Deposit not found."}, 404
+        
         return deposit
     
 
