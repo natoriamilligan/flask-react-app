f85f5eb8e8d26f392593a6e03718d64c85a04271
Natoria Milligan <natoriaray@gmail.com>
2026-02-05 11:41:45 -0600

    added new functions and modals to handle deleteing accoutns and creating fresh token

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -1,7 +1,6 @@
 import { useEffect, useState } from 'react';
-import { Card, Button, Form } from 'react-bootstrap';
+import { Card, Button, Form, Modal} from 'react-bootstrap';
 import { PersonCircle } from 'react-bootstrap-icons';
-import { jwtDecode } from 'jwt-decode';
 import Sidebar from './Sidebar';
 import Header from './Header';
 import '../Profile.css';
@@ -11,18 +10,20 @@ function Profile() {
     const [lastname, setLastname] = useState('');
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [loginUsername, setLoginUsername] = useState('');
+    const [loginPassword, setLoginPassword] = useState('');
     const [isDisabled, setIsDisabled] = useState(true);
     const [active, setActive] = useState(false);
     const [passAlert, setPassAlert] = useState(false);
     const [isDelete, setIsDelete] = useState(false);
+    const [loginAlert, setLoginAlert] = useState(false);
 
-    const token = localStorage.getItem("accessToken");
-    let accountId = null;
+    const [loginShow, setLoginShow] = useState(false);
+    const [deleteShow, setDeleteShow] = useState(false);
+    const [changePasswordShow, setChangePasswordShow] = useState(false);
 
-    if (token) {
-        const decodedToken = jwtDecode(token);
-        accountId = decodedToken.sub || decodedToken.identity;
-    }
+    const handleLoginClose = () => setLoginShow(false);
+    const handleDeleteClose = () => setDeleteShow(false);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -40,23 +41,22 @@ function Profile() {
                 setLastname(data.last_name || "");
                 setUsername(data.username || "");
             } else {
-                return alert("Please try again.");
+                return alert("Error fetching data from database.");
             }
             
         }
         fetchData();
     }, []);
+
+    const loginReturn = () => {
+        navigate("/login");
+    };
     
     const handleSubmit = async (e) => {
         e.preventDefault();
         if (isDelete) {
-            const response = await fetch(`https://api.banksie.app/account/${accountId}`, {
-                method: 'DELETE',
-                headers: { 
-                    'Content-Type' : 'application/json',
-                    Authorization : `Bearer ${token}` 
-                }
-            })
+            setLoginShow(true)
+            setIsDelete(false)
         } else {
             if (password == "") {
             setPassAlert(true);
@@ -78,12 +78,126 @@ function Profile() {
                 setActive(!active);
             }  
         }
-    }
+    };
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        if (username == "" || password == "") {
+            setLoginAlert(true);
+        } else {
+            try {
+                const response = await fetch('https://api.banksie.app/login', {
+                        method: 'POST',
+                        headers: { 'Content-Type' : 'application/json' },
+                        body: JSON.stringify({
+                            username: loginUsername,
+                            password: loginPassword 
+                    })
+                })
+
+                if (response.ok) {
+                    setLoginShow(false);
+                    setDeleteShow(true);
+                } else {
+                    alert("Login unsuccessful. Please try again.")
+                }
+            } catch {
+                alert("Something wrong with the server");
+            }
+        }
+    };
+
+    const handleLogout = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await fetch('https://api.banksie.app/logout', {
+                method: 'POST',
+                headers: { 
+                    'Content-Type' : 'application/json'
+                }
+            })
+
+            if (!response.ok) {
+                throw new error("Logout failed.")
+            }
+
+            loginReturn();
+        } catch {
+            alert("There was an error logging out.");
+        }
+    };
+
+    const handleDelete = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await fetch(`https://api.banksie.app/account/${accountId}`, {
+                method: 'DELETE',
+                headers: { 
+                    'Content-Type' : 'application/json'
+                }
+            })
+
+            if (!response.ok) {
+                throw new Error("Deletion failed.")
+            }
+
+            setDeleteShow(false);
+            handleLogout();
+        } catch {
+            alert("Error deleting account. Please try again");
+            setDeleteShow(false);
+        }
+        
+    };
 
     return (
         <>
             <Sidebar />
             <Header />
+            <Modal show={loginShow} onHide={handleLoginClose}>
+                <Modal.Header>
+                    <Modal.Title>Delete Account</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <p>Please enter your account credentials.</p>
+
+                    <Form onSubmit={handleLogin}>
+                        <Form.Group controlId='username'>
+                            <Form.Label>Username:</Form.Label>
+                            <Form.Control 
+                                type='text' 
+                                value={loginUsername}
+                                onChange={(e) => setLoginUsername(e.target.value)}
+                            />
+                        </Form.Group>
+                        <Form.Group controlId='password'>
+                            <Form.Label>Password:</Form.Label>
+                            <Form.Control 
+                                type='text' 
+                                value={loginPassword}
+                                onChange={(e) => setLoginPassword(e.target.value)}
+                            />
+                            {loginAlert &&
+                                <Form.Text>Please enter username and password.</Form.Text>
+                            }
+                        </Form.Group>
+                        <div className="login-btn">
+                            <Button type="submit">Submit</Button>
+                        </div>
+                    </Form>
+                </Modal.Body>
+            </Modal> 
+            <Modal show={deleteShow} onHide={handleDeleteClose}>
+                <Modal.Header>
+                    <Modal.Title>Delete Account</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <p>Are you sure you want to delete your account?</p>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button onClick={handleDelete}>Delete</Button>
+                </Modal.Footer>
+            </Modal>  
             <Card className="profile-card">
                 <Card.Body>
                     <div className="name-section">
