8c9f82351a43ee2b3721e0a7659d95ccd3f1229d
Natoria Milligan <natoriaray@gmail.com>
2026-02-11 14:30:12 -0600

    added session timer to prompt user to continue or terminate session

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Container, Button } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import Login from './components/login';
@@ -11,16 +11,32 @@ import Deposit from './components/Deposit';
 import Withdrawal from './components/Withdrawal';
 import Transfer from './components/Transfer';
 import Home from './components/Home';
+import RefreshSession from './components/RefreshSession';
+
+
 
 function App() {
+  const [showModal, setShowModal] = useState(false);
+  const [loginTime, setLoginTime] = useState(null);
+  
+  useEffect(() => {
+    if (!loginTime) return;
+
+    const timer = setTimeout(function(){
+      setShowModal(true);
+    }, 900000 - 30000);
+
+    return () => clearTimeout(timer);
+  }, [loginTime])
 
   return (
     <Container fluid className="px-0">
       <Toaster position="top-right" />
+      {showModal && <RefreshSession show={showModal} setShowModal={setShowModal} setLoginTime={setLoginTime} />}
       <Router>
         <Routes>
           <Route path='/' element={<Home />} />
-          <Route path='/login' element={<Login />} />
+          <Route path='/login' element={<Login setLoginTime={setLoginTime}/>} />
           <Route path='/create' element={<Create />} />
           <Route path='/logout' element={<Logout />} />
           <Route path='/dashboard' element={<Dashboard />} />
