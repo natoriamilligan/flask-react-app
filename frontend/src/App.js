240bd4a40f3519231792996e3818ccc5ca2255f7
Natoria Milligan <natoriaray@gmail.com>
2026-02-12 10:07:53 -0600

    added component to handle the events from the refresh modal

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,7 @@
 import { useState, useEffect } from 'react';
 import { Container } from 'react-bootstrap';
 import { useNavigate, BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Toaster } from 'react-hot-toast';
-import { toast } from 'react-hot-toast;'
+import { Toaster, toast } from 'react-hot-toast';
 import Login from './components/login';
 import Create from './components/Create';
 import Logout from './components/Logout';
@@ -12,66 +11,17 @@ import Deposit from './components/Deposit';
 import Withdrawal from './components/Withdrawal';
 import Transfer from './components/Transfer';
 import Home from './components/Home';
-import RefreshSession from './components/RefreshSession';
+import SessionManager from './components/SessionManager';
 import './App.css';
 
-
-
 function App() {
-  const [showModal, setShowModal] = useState(false);
   const [loginTime, setLoginTime] = useState(null);
-  const [noResponse, setNoResponse] = useState(false);
-
-  const navigate = useNavigate();
-
-  const logoutReturn = () => {
-    navigate("/logout");
-  };
-  
-  useEffect(() => {
-    if (!loginTime) return;
-
-    const timer = setTimeout(function(){
-      setShowModal(true);
-    }, 60000 - 40000);
-
-    const timer2 = setTimeout(function(){
-      if (noResponse) {
-        logoutSession();
-      }
-    }, 60000 - 10000);
-
-    return () => {
-      clearTimeout(timer);
-      clearTimeout(timer2);
-    };
-  }, [loginTime])
-
-  const logoutSession = async () => {
-      try {
-          const response = await fetch('http://localhost:5000/logout', {
-              method: 'POST',
-              headers: {'Content-Type' : 'application/json'},
-              credentials: "include"
-          })
-
-          const data = await response.json();
-
-          if (!response.ok) {
-              throw new Error(data.message || `Failed to fetch: ${response.status} ${response.statusText}`)
-          }
-
-          setLoginTime(null);
-          logoutReturn();
-      } catch(error) {
-          toast.error(error.message)
-      }
-  };
 
   return (
     <Container fluid className="px-0">
       <Toaster position="top-right" />
       <Router>
+        <SessionManager loginTime={loginTime} setLoginTime={setLoginTime} />
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/login' element={<Login setLoginTime={setLoginTime}/>} />
@@ -83,7 +33,6 @@ function App() {
           <Route path='/withdrawal' element={<Withdrawal />} />
           <Route path='/transfer' element={<Transfer />} />
         </Routes>
-        {showModal && <RefreshSession showModal={showModal} setShowModal={setShowModal} setLoginTime={setLoginTime} setNoResponse={setNoResponse} />}
       </Router>
     </Container>
   );
---
+++ frontend/src/components/SessionManager.js
@@ -0,0 +1,60 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from 'react-hot-toast';
+import RefreshSession from "./RefreshSession";
+
+function SessionManager({loginTime, setLoginTime}) {
+    const [showModal, setShowModal] = useState(false);
+    const navigate = useNavigate();
+
+    const logoutReturn = () => {
+        navigate("/logout");
+    };
+    
+    useEffect(() => {
+        if (!loginTime) return;
+
+        const timer = setTimeout(function(){
+            setShowModal(true);
+        }, 60000 - 40000);
+
+        const timer2 = setTimeout(function(){
+            logoutSession();
+        }, 60000 - 10000);
+
+        return () => {
+            clearTimeout(timer);
+            clearTimeout(timer2);
+        };
+    }, [loginTime])
+
+    const logoutSession = async () => {
+        try {
+            const response = await fetch('http://localhost:5000/logout', {
+                method: 'POST',
+                headers: {'Content-Type' : 'application/json'},
+                credentials: "include"
+            })
+
+            const data = await response.json();
+
+            if (!response.ok) {
+                throw new Error(data.message || `Failed to fetch: ${response.status} ${response.statusText}`)
+            }
+
+            setShowModal(false);
+            setLoginTime(null);
+            logoutReturn();
+        } catch(error) {
+            toast.error(error.message)
+        }
+    };
+
+    return (
+        <>
+            {showModal && <RefreshSession showModal={showModal} setShowModal={setShowModal} setLoginTime={setLoginTime} />}
+        </>
+        
+    )
+}
+export default SessionManager
