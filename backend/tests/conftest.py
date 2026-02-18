6f5e81ea005b091544491f7e8d889f11bf60b066
Natoria Milligan <46578731+natoriamilligan@users.noreply.github.com>
2026-02-18 12:35:30 -0600

    changed app fixture to spin up postgres database instead of sqlite

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,18 +1,20 @@
+import os
 import pytest
+from flask_migrate import upgrade
 from app import create_app
 from db import db
-
-BASE_URL = "http://localhost:5000"
 
 @pytest.fixture
 def app():
     test_app = create_app("testing")
 
-    with test_app.app_contect():
-        db.create_all()
+    db_url = os.getenv["DATABASE_URL"]
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url
+
+    with test_app.app_context():
+        upgrade()
         yield test_app
         db.session.remove()
-        db.drop_all()
 
 @pytest.fixture
 def client(app):
