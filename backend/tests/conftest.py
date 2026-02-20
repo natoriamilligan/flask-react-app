a4eece68863ec0b72a51dbecda37e8178922e68e
Natoria Milligan <natoriaray@gmail.com>
2026-02-19 22:59:47 -0600

    added configuration for test user to configuration file

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,21 +1,63 @@
 import os
+from passlib.hash import pbkdf2_sha256
 import pytest
 from flask_migrate import upgrade
 from app import create_app
+from models import AccountModel
 from db import db
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def app():
     test_app = create_app("testing")
 
-    db_url = os.getenv["DATABASE_URL"]
-    app.config["SQLALCHEMY_DATABASE_URI"] = db_url
+    db_url = os.getenv("DATABASE_URL")
+    test_app.config["SQLALCHEMY_DATABASE_URI"] = db_url
 
     with test_app.app_context():
         upgrade()
         yield test_app
         db.session.remove()
 
+@pytest.fixture(autouse=True)
+def session(app):
+    db.session.begin()
+    yield
+    db.session.rollback()
+
 @pytest.fixture
 def client(app):
     return app.test_client()
+
+@pytest.fixture(scope="session")
+def test_user(app):
+    with app.app_context():
+        existingUser = AccountModel.query.filter_by(username="test_user").first()
+        if existingUser:
+            existingUser.password = pbkdf2_sha256.hash("bunnyB45!!!")
+            db.session.commit()
+            return {
+                "username": existingUser.username,
+                "password": "bunnyB45!!!"
+            }
+        
+        test_user = AccountModel(
+            first_name="Test",
+            last_name="User",
+            username="test_user",
+            password=pbkdf2_sha256.hash("bunnyB45!!!")
+        )
+    
+        db.session.add(test_user)
+        db.session.commit()
+        return {
+            "username": test_user.username,
+            "password": "bunnyB45!!!"
+        }
+
+@pytest.fixture()
+def me(client, test_user):
+    client.post("/login", json={
+        "username": test_user.username,
+        "password": "bunnyB45!!!"
+    })
+    return client.get("/me").get_json()
