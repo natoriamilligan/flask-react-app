4c2510f6c1241f3e0ee9fcb98880e685b0e0e4be
Natoria Milligan <46578731+natoriamilligan@users.noreply.github.com>
2026-02-27 14:33:14 -0600

    added assume role policy for ecs tasks and AWS identity data block

--- terraform/core-infra/data.tf
+++ terraform/core-infra/data.tf
@@ -23,3 +23,18 @@ data "aws_iam_policy_document" "origin_bucket_policy" {
       }
     }
 }
+
+# Task execution role data
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+# Provides data about the current authenticated AWS identity
+data "aws_caller_identity" "current" {}
