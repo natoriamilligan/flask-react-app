8642918d843457e02f6a4d059a202fa2d425f0ca
Natoria Milligan <natoriaray@gmail.com>
2026-02-27 15:08:14 -0600

    removed button for accessing diagrams

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,27 +1,30 @@
-import { Button } from 'react-bootstrap';
-import { useNavigate } from 'react-router-dom';
-import '../Home.css';
+import { Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import "../Home.css";
 
 function Home() {
-const navigate = useNavigate();
+  const navigate = useNavigate();
 
-const toLogin = () => {
-    navigate("/Login")
-}
+  const toLogin = () => {
+    navigate("/Login");
+  };
 
-const toCreate = () => {
-    navigate("/Create")
-}
+  const toCreate = () => {
+    navigate("/Create");
+  };
 
-    return (
-        <div className="wrapper">
-            <h1>BANKSIE</h1>
-            <div className="btn-wrapper">
-                <Button className="front-btns" onClick={toLogin}>Login</Button>
-                <Button className="front-btns" onClick={toCreate}>Create Account</Button>
-                <Button className="front-btns">See Architecture Diagrams</Button>
-            </div>
+  return (
+    <div className="wrapper">
+      <h1>BANKSIE</h1>
+      <div className="btn-wrapper">
+        <Button className="front-btns" onClick={toLogin}>
+          Login
+        </Button>
+        <Button className="front-btns" onClick={toCreate}>
+          Create Account
+        </Button>
       </div>
-    )
+    </div>
+  );
 }
-export default Home
+export default Home;
