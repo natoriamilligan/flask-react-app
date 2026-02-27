b31eacf5c4a77427e32626fa74e31a063951c6f1
Natoria Milligan <natoriaray@gmail.com>
2026-02-26 20:08:55 -0600

    copied lambda function from eks-terraform-project repo

---
+++ lambda/ns-propagation.py
@@ -0,0 +1,44 @@
+import os
+import json
+import boto3
+import requests
+import dns.resolver
+from dns.exception import DNSException
+
+BOT_SECRET_NAME = os.environ["SLACK_BOT_TOKEN_SECRET"]
+NAMESERVERS = json.loads(os.environ["NAMESERVERS"])
+DOMAIN = os.environ["DOMAIN"]
+SCHEDULER_NAME = os.environ["SCHEDULER_NAME"]
+
+secrets_client = boto3.client("secretsmanager")
+scheduler_client = boto3.client("scheduler")
+
+def get_url_secret():
+    response = secrets_client.get_secret_value(SecretId=BOT_SECRET_NAME)
+    url_string = response.get("SecretString")
+    if not url_string:
+        raise RuntimeError("Cannot find SecretString key.")
+    return json.loads(url_string)["slack-bot-token"]
+
+def lambda_handler(event, context):
+    token = get_url_secret()
+    
+    propagated = True
+    for ns in NAMESERVERS:
+        try:
+            resolver = dns.resolver.Resolver()
+            resolver.nameservers = [ns]
+            resolver.resolve(DOMAIN, "NS")
+        except DNSException:
+            propagated = False
+            break
+
+    if propagated:
+        message = f"Nameservers have propagated for {DOMAIN}."
+        requests.post(SLACK_WEBHOOK_URL, json={"text": message})
+        scheduler_client.update_schedule(Name=SCHEDULER_NAME, State="DISABLED")
+    
+    return {
+        "status_code": 200,
+        "body": json.dumps({"propagated": propagated})
+    }
