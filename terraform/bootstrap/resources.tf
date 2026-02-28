8e4ebc14954a906201d74dafdddf27da3b22c7e3
Natoria Milligan <natoriaray@gmail.com>
2026-02-28 17:55:22 -0600

    lambda file from zip to py file

--- terraform/bootstrap/resources.tf
+++ terraform/bootstrap/resources.tf
@@ -58,11 +58,11 @@ resource "aws_iam_role_policy" "lambda_policy" {
 
 # Create lambda function
 resource "aws_lambda_function" "lambda_function" {
-  filename         = "ns-propagation.zip"
+  filename         = "ns-propagation.py"
   function_name    = "ns-propagation"
   role             = aws_iam_role.lambda_role.arn
   handler          = "ns-propagation.lambda_handler"
-  source_code_hash = filebase64sha256("../../lambda/ns-propagation.zip")
+  source_code_hash = filebase64sha256("../../lambda/ns-propagation.py")
   runtime          = "python3.11"
 
   environment {
