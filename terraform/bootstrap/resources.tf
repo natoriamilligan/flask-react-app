58019d038b8843cb2b45f6da22c4f7e19254901e
Natoria Milligan <natoriaray@gmail.com>
2026-02-28 16:08:12 -0600

    fixed type for state lock configuration

--- terraform/bootstrap/resources.tf
+++ terraform/bootstrap/resources.tf
@@ -4,7 +4,7 @@ terraform {
     bucket         = "nmilligan-tf-states"
     key            = "bootstrap/terraform.tfstate"
     region         = "us-east-1"
-    use_lockfile   = "terraform-lock"
+    use_lockfile   = true
     encrypt        = true
   }
 }
