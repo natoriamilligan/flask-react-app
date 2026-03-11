b809375e53ea429045c1719aa2c8dab938ebe8d4
Natoria Milligan <natoriaray@gmail.com>
2026-03-10 22:18:01 -0500

    added outputs for vpx and subnets

--- terraform/core-infra/outputs.tf
+++ terraform/core-infra/outputs.tf
@@ -1,3 +1,11 @@
 output "db_secret" {
   value = aws_secretsmanager_secret.db_secret.arn
 }
+
+output "vpc" {
+  value = aws_vpc.main.id
+}
+
+output "public_subnet" {
+  value = aws_subnet.public_a.id
+}
