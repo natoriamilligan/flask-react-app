d9974edd1b8fe32eef8e3cd70cc88014cd2f8461
Natoria Milligan <natoriaray@gmail.com>
2026-03-11 22:21:47 -0500

    changed styling

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,118 +1,148 @@
-import { useState, useRef, useEffect } from 'react';
-import { Stack, Nav, Button } from 'react-bootstrap';
-import {NavLink, useLocation} from 'react-router-dom';
-import { useNavigate } from 'react-router-dom';
-import toast from 'react-hot-toast';
-import '../Sidebar.css';
+import { useState, useRef, useEffect } from "react";
+import { Stack, Nav, Button } from "react-bootstrap";
+import { NavLink, useLocation } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
+import "../Sidebar.css";
 
 function Sidebar() {
-    const [active, setActive] = useState(false);
-    const [open, setOpen] = useState(false);
-    const navRef = useRef(null)
-    const location =useLocation();
-    const navigate = useNavigate();
+  const [active, setActive] = useState(false);
+  const [open, setOpen] = useState(false);
+  const navRef = useRef(null);
+  const location = useLocation();
+  const navigate = useNavigate();
 
-    const handleLogout = async () => {
-        try {
-            const response = await fetch("https://api.banksie.app/logout", {
-                method: 'POST',
-                headers: {'Content-Type' : 'application/json'},
-                credentials: "include"
-            })
+  const handleLogout = async () => {
+    try {
+      const response = await fetch("https://api.banksie.app/logout", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        credentials: "include",
+      });
 
-            const data = await response.json();
+      const data = await response.json();
 
-            if (!response.ok) {
-                throw new Error(data.message || `Failed to fetch: ${response.status} ${response.statusText}`)
-            }
+      if (!response.ok) {
+        throw new Error(
+          data.message ||
+            `Failed to fetch: ${response.status} ${response.statusText}`,
+        );
+      }
 
-            navigate("/logout");
-        } catch(error) {
-            toast.error(error.message)
-        }
-        
+      navigate("/logout");
+    } catch (error) {
+      toast.error(error.message);
     }
-    
-    const toggleNav = (e) => {
-        e.preventDefault();
-        setOpen(!open);
-        setActive(!active);
-    }
-    
-    const clickOutside = (e) => {
-        if (navRef.current && !navRef.current.contains(e.target)) {
-            setOpen(false);
-            setActive(false);
-        }
-        
-    }
+  };
 
-    useEffect(() => {
-        if (open) {
-            document.addEventListener("mousedown", clickOutside);
-        }
-        return () => {
-            document.removeEventListener('mousedown', clickOutside);
-        }
-    }, [open])
+  const toggleNav = (e) => {
+    e.preventDefault();
+    setOpen(!open);
+    setActive(!active);
+  };
 
-    if (active) {
-        document.body.classList.add("no-scroll");
-    } else {
-        document.body.classList.remove('no-scroll');
+  const clickOutside = (e) => {
+    if (navRef.current && !navRef.current.contains(e.target)) {
+      setOpen(false);
+      setActive(false);
     }
-    
+  };
 
-    return (
-        <div>
-            {open && <div className="overlay" onClick={() => setOpen(false)}></div>}
-            <Nav ref={navRef}>
-                <a id="nav-toggle" href="" onClick={toggleNav}>
-                    <span className="nav-bar"></span>
-                    <span className="nav-bar"></span>
-                    <span className="nav-bar"></span>
-                </a>
-                <Stack gap={2} id="nav-stack" className={open ? "active" : 'wrapper'}>
-                    <Nav.Item className={location.pathname === "/dashboard" ? "nav-items" : ""}>
-                        <Nav.Link 
-                        as={NavLink}
-                        to="/dashboard" 
-                        className={location.pathname === "/dashboard" ? "nav-link-active nav-links fs-6" : "nav-links fs-6"}
-                        >
-                        Home
-                        </Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item className={location.pathname === "/deposit" ? "nav-items" : ""}>
-                        <Nav.Link 
-                        as={NavLink}
-                        to="/deposit" 
-                        className={location.pathname === "/deposit" ? "nav-link-active nav-links fs-6" : "nav-links fs-6"}
-                        >
-                        Deposit Funds
-                        </Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item className={location.pathname === "/withdrawal" ? "nav-items" : ""}>
-                        <Nav.Link 
-                        as={NavLink}
-                        to="/withdrawal" 
-                        className={location.pathname === "/withdrawal" ? "nav-link-active nav-links fs-6" : "nav-links fs-6"}
-                        >
-                        Withdrawal Funds
-                        </Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item className={location.pathname === "/transfer" ? "nav-items" : ""}>
-                        <Nav.Link 
-                        as={NavLink}
-                        to="/transfer" 
-                        className={location.pathname === "/transfer" ? "nav-link-active nav-links fs-6" : "nav-links fs-6"}
-                        >
-                        Send Transfer
-                        </Nav.Link>
-                    </Nav.Item>
-                    <Button className="logout-btn" variant="danger" onClick={handleLogout}>Logout</Button>
-                </Stack>
-            </Nav>
-        </div>
-    )
+  useEffect(() => {
+    if (open) {
+      document.addEventListener("mousedown", clickOutside);
+    }
+    return () => {
+      document.removeEventListener("mousedown", clickOutside);
+    };
+  }, [open]);
+
+  if (active) {
+    document.body.classList.add("no-scroll");
+  } else {
+    document.body.classList.remove("no-scroll");
+  }
+
+  return (
+    <div>
+      {open && <div className="overlay" onClick={() => setOpen(false)}></div>}
+      <Nav ref={navRef}>
+        <a id="nav-toggle" href="" onClick={toggleNav}>
+          <span className="nav-bar"></span>
+          <span className="nav-bar"></span>
+          <span className="nav-bar"></span>
+        </a>
+        <Stack gap={2} id="nav-stack" className={open ? "active" : "wrapper"}>
+          <Nav.Item
+            className={location.pathname === "/dashboard" ? "nav-items" : ""}
+          >
+            <Nav.Link
+              as={NavLink}
+              to="/dashboard"
+              className={
+                location.pathname === "/dashboard"
+                  ? "nav-link-active nav-links fs-6"
+                  : "nav-links fs-6"
+              }
+            >
+              Home
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item
+            className={location.pathname === "/deposit" ? "nav-items" : ""}
+          >
+            <Nav.Link
+              as={NavLink}
+              to="/deposit"
+              className={
+                location.pathname === "/deposit"
+                  ? "nav-link-active nav-links fs-6"
+                  : "nav-links fs-6"
+              }
+            >
+              Deposit Funds
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item
+            className={location.pathname === "/withdrawal" ? "nav-items" : ""}
+          >
+            <Nav.Link
+              as={NavLink}
+              to="/withdrawal"
+              className={
+                location.pathname === "/withdrawal"
+                  ? "nav-link-active nav-links fs-6"
+                  : "nav-links fs-6"
+              }
+            >
+              Withdrawal Funds
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item
+            className={location.pathname === "/transfer" ? "nav-items" : ""}
+          >
+            <Nav.Link
+              as={NavLink}
+              to="/transfer"
+              className={
+                location.pathname === "/transfer"
+                  ? "nav-link-active nav-links fs-6"
+                  : "nav-links fs-6"
+              }
+            >
+              Send Transfer
+            </Nav.Link>
+          </Nav.Item>
+          <Button
+            className="logout-btn"
+            variant="danger"
+            onClick={handleLogout}
+          >
+            Logout
+          </Button>
+        </Stack>
+      </Nav>
+    </div>
+  );
 }
-export default Sidebar
+export default Sidebar;
