954bb40c4c46b492ab321eaf06b7c192cc73260f
Natoria Milligan <46578731+natoriamilligan@users.noreply.github.com>
2026-04-14 10:43:28 -0500

    Reduce CPU and memory for Fargate task

--- terraform/core-infra/resources.tf
+++ terraform/core-infra/resources.tf
@@ -427,8 +427,8 @@ resource "aws_ecs_task_definition" "app_task" {
   execution_role_arn       = aws_iam_role.task_execution_role.arn
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
-  cpu                      = "1024" 
-  memory                   = "4096"
+  cpu                      = "256" 
+  memory                   = "512"
   container_definitions    = jsonencode([
     {
       name      = "app-container"
